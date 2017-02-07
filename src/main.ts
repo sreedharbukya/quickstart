40adea21b9be671b04258fdee421ec4d36c46175
Sreedhar Bukya <sreedhar.bukya@gmail.com>
2017-02-07 22:53:24 +0530

    angular 2 in aot  mode

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
-import {platformBrowser}    from '@angular/platform-browser';
-import {AppModuleNgFactory} from '../aot/src/app/app.module.ngfactory';
-console.log('Running AOT compiled');
-platformBrowser().bootstrapModuleFactory(AppModuleNgFactory);
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { AppModule } from './app/app.module';
+
+platformBrowserDynamic().bootstrapModule(AppModule);
