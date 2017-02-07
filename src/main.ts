6dae0b3a1213c2e51ac7b4d6dcf96eba76b1326e
sreedhar <sreedhar@kisanhub.com>
2017-02-07 21:36:17 +0530

    intermediate setup for aot angular 2

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-import { AppModule } from './app/app.module';
-
-platformBrowserDynamic().bootstrapModule(AppModule);
+import {platformBrowser}    from '@angular/platform-browser';
+import {AppModuleNgFactory} from '../aot/src/app/app.module.ngfactory';
+console.log('Running AOT compiled');
+platformBrowser().bootstrapModuleFactory(AppModuleNgFactory);
