83a044f9bf7bdc5abbb3e6097150892dff2d4b91
Sreedhar Bukya <sreedhar.bukya@gmail.com>
2017-02-08 12:16:47 +0530

    SB: updated minor changes

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
-
+console.log("App is running from JIT");
 platformBrowserDynamic().bootstrapModule(AppModule);
